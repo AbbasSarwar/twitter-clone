b619af5a6b654d12a6a89c8bd4596b4d50126b13
Abbas Sarwar <abbassarwar100@gmail.com>
2023-09-14 12:51:15 +0500

    Changes to top bar

--- src/Feed/TopBar.js
+++ src/Feed/TopBar.js
@@ -23,14 +23,14 @@ const TopBar = ({
         </div>
       </div>
       <div className="d-flex flex-row">
-        <div className="box d-flex  justify-content-center align-items-center flex-fill w-100 fs-6 fw-semibold">
-          <div className="d-flex align-items-center justify-content-center m-0 h-100 w-100"><p className="rounded-border border-bottom border-primary border-3 d-flex align-items-center h-100">{bTitle1}</p></div>
+        <div className="box d-flex justify-content-center align-items-center flex-fill w-100 fs-6 fw-semibold">
+          <div className="d-flex align-items-center justify-content-center m-0 h-100 w-100"><p className="rounded-border border-bottom border-primary border-3 d-flex justify-content-center align-items-center h-100 mt-2">{bTitle1}</p></div>
         </div>
-        <div className="box flex-fill py-3 d-flex justify-content-center align-items-center w-100 fs-6 fw-semibold">
-          <div className="p-0 d-flex align-items-center justify-content-center m-0 h-100 w-100"><p className="d-flex align-items-center h-100">{bTitle2}</p></div>
+        <div className="box flex-fill py-2 d-flex justify-content-center align-items-center w-100 fs-6 fw-semibold">
+          <div className="p-0 d-flex align-items-center justify-content-center m-0 h-100 w-100"><p className="d-flex justify-content-center align-items-center h-100 mt-2">{bTitle2}</p></div>
         </div>
         {bTitle3 ? (
-          <div className="box flex-fill text-center py-3 w-100 fs-6 fw-semibold">
+          <div className="box flex-fill text-center py-2 w-100 fs-6 fw-semibold">
             <p className="m-0">{bTitle3}</p>
           </div>
         ) : null}
