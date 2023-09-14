5b5f5ec7ffe489b4d510a134adac1d9642242a3f
Abbas Sarwar <abbassarwar100@gmail.com>
2023-09-14 14:50:11 +0500

    Test checkut

--- src/Sidebar/OptionMenu.js
+++ src/Sidebar/OptionMenu.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const OptionMenu = ({dir, text, icon}) => {
     return (
-        <Link title={text} href={`${dir}`}>{icon}<span className="btn-text">{text}</span></Link>
+        <Link title={text} to={`${dir}`}>{icon}<span className="btn-text">{text}</span></Link>
     )
 }
 export default OptionMenu;
